3f806b69315332b02cd1d23471f67ad4a4af6c9d
Mithun <mithuncm@gmail.com>
2023-04-10 18:58:08 +0530

    Refactoring and optimizing v1.01

--- src/main/java/io/uberkautilya/UFOEnemyShipBuilding.java
+++ src/main/java/io/uberkautilya/UFOEnemyShipBuilding.java
@@ -12,23 +12,24 @@ public class UFOEnemyShipBuilding extends EnemyShipBuilding {
         // what types of weapons and engines a regular UFO
         // needs. The EnemyShip object is returned & given a name
 
-        if(typeOfShip.equals("UFO")){
-            EnemyShipFactory shipPartsFactory = new UFOEnemyShipFactory();
-            enemyShip = new UFOEnemyShip(shipPartsFactory);
-            enemyShip.setName("UFO Grunt Ship");
+        switch (typeOfShip) {
+            case "UFO" -> {
+                EnemyShipFactory shipPartsFactory = new UFOEnemyShipFactory();
+                enemyShip = new UFOEnemyShip(shipPartsFactory);
+                enemyShip.setName("UFO Grunt Ship");
 
-        } else
+            }
 
             // If UFO BOSS was sent grab use the factory that knows
             // what types of weapons and engines a Boss UFO needs.
             // The EnemyShip object is returned & given a name
-
-            if(typeOfShip.equals("UFO BOSS")){
+            case "UFO BOSS" -> {
                 EnemyShipFactory shipPartsFactory = new UFOBossEnemyShipFactory();
                 enemyShip = new UFOBossEnemyShip(shipPartsFactory);
                 enemyShip.setName("UFO Boss Ship");
 
             }
+        }
 
         return enemyShip;
     }
